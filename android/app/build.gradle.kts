755ed0d6b1a2593fec91ddc03d1c4ac8dcd39f99
PADMORE YEBOAH <95287415+MAD-MORE@users.noreply.github.com>
2026-09-01 16:03:41 +0000

    fix: migrate Kotlin JVM target to compilerOptions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -18,7 +18,16 @@ android {
 
     buildFeatures { compose = true }
 
-    kotlinOptions { jvmTarget = "17" }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
+    }
 
     packaging { resources.excludes += "/META-INF/{AL2.0,LGPL2.1}" }
 }
